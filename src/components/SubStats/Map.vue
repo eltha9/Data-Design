c56ab624fb82676c33935d8ad34a0dcc8bfafcee
Théa D <theadosto@gmail.com>
2021-04-12 00:53:28 +0200

    wip map

--- src/components/SubStats/Map.vue
+++ src/components/SubStats/Map.vue
@@ -25,13 +25,49 @@
 
         </div>
         <div class="map flex-1 min-h-full">
-
+            <LMap :zoom="zoom" :center="center">
+                <LTileLayer :url="url"></LTileLayer>
+                <!-- <LMarker :lat-lng="[13.1333682,77.5651881]"></LMarker> -->
+                <LMarker :lat-lng="[47.068569, 2.365135]" >
+                    <LIcon :icon-anchor="staticAnchor" style="width:30px; height:30px">
+                    <div class="headline">{{customText}}</div>
+                    </LIcon>
+                </LMarker>
+            </LMap>
         </div>
 
     </section>
 </template>
 
-<script setup>
+<script>
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer, LMarker, LIcon} from "@vue-leaflet/vue-leaflet";
+import { icon } from "leaflet";
+
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      zoom: 6,
+      center: [47.068569,2.365135],
+      bounds: null,
+    //   icon: icon({
+    //     iconUrl: "../assets/message-square.png",
+    //     iconSize: [32, 37],
+    //     iconAnchor: [16, 37]
+    //   }),
+      staticAnchor: [16, 37],
+      customText: "Foobar",
+      iconSize: 64
+    };
+  }
+};
 
 </script>
 
