50c06b9f31664a2026c36eb688d8dc79fb779421
eltha9 <theadosto@gmail.com>
2021-04-12 09:07:38 +0200

    wip graph

--- src/components/SubStats/Graph.vue
+++ src/components/SubStats/Graph.vue
@@ -36,8 +36,11 @@
                 
             </div>
         </div>
-        <div class="graph w-12/12 ">
-
+        <div class="graph w-12/12 hidden">
+           
+        </div>
+        <div class="graph-fake w-12/12 ">
+           <img src="../../assets/fake_graph.svg" alt="">
         </div>
     </section>
 </template>
@@ -45,8 +48,8 @@
 <script>
 import * as d3 from "d3"
 
-
 export default{
+    poData:[10,20,30,25],
     mounted:function () {
 
         let container = document.querySelector('#graph .graph')
@@ -57,6 +60,43 @@ export default{
             .attr("height", '100%')
             .attr('viewBox','0 0 '+Math.min(container.offsetWidth,container.offsetHeight)+' '+Math.min(container.offsetWidth, container.offsetHeight))
             .attr('preserveAspectRatio','xMinYMin')
+
+        var xscale = d3.scaleLinear()
+            .domain([0, 30])
+            .range([0, container.offsetWidth - 100]);
+        // let startDate = new Date('01/01/2020')
+        // let endDate = new Date ('01/03/2021')
+        // var xscale = d3.scaleTime()
+        //     .domain([startDate.getMonth(), endDate.getMonth()] )
+        //     .range([startDate.getMonth(), endDate.getMonth()])
+        //     // .nice()z
+
+        var yscale = d3.scaleLinear()
+                .domain([0, 300])
+                .range([container.offsetHeight-30, 0]);
+
+        var x_axis = d3.axisBottom()
+                .scale(xscale);
+                // .tickFormat(function(d){
+                //     var seconds = d; 
+                // var final_seconds = seconds % 60;
+                // var minutes = (seconds - final_seconds) / 60;
+                // var tick_text = minutes.toString() + ":" + ("0" + final_seconds).slice(-2);
+                //     return tick_text;
+                // });
+
+        var y_axis = d3.axisLeft()
+                .scale(yscale);
+
+            graph.append("g")
+            .attr("transform", "translate(50, 10)")
+            .call(y_axis);
+
+        var xAxisTranslate = container.offsetHeight-30 + 10;
+
+        graph.append("g")
+        .attr("transform", "translate(50, " + xAxisTranslate  +")")
+        .call(x_axis)
         // graph.append("rect")
         //     .attr("x",20)
         //     .attr("y",20)
@@ -64,7 +104,8 @@ export default{
             // .attr("height", '100%')
 
         
-    }
+    },
+    
 }
 </script>
 
@@ -80,13 +121,17 @@ export default{
     margin-right: 1.5rem;
 }
 
-.graph{
+.graph, .graph-fake{
     margin-top: 4.5rem;
     height: 492px;
 }
 
 section#graph .graph svg{
     width: 100%;
+    height: 100%;
+}
+section#graph .graph-fake img{
+    margin: auto;
     height: 100%;
 }
 
